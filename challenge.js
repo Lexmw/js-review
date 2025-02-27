4244ef86d61e2dd57acb168a74c56371aab9a8c1
Lexius Waltar <lexius.waltar@gmail.com>
2025-02-26 22:59:37 -0500

    Create challenge.js

---
+++ challenge.js
@@ -0,0 +1,7 @@
+// challenge.js
+
+// Challenge: Number Guessing Game
+// Create a simple number guessing game using loops, DOM manipulation, and conditionals.
+// The user must guess a number between 1 and 10. 
+// The program will give hints if the guess is too high or too low.
+// The user wins when they guess the correct number.
