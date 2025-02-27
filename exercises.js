88616ae5ccb9be1873f3958176b2f82316456f29
Lexius Waltar <lexius.waltar@gmail.com>
2025-02-26 22:57:02 -0500

    Create exercises.js

---
+++ exercises.js
@@ -0,0 +1,29 @@
+// 1. Click Counter
+// Create a button. When clicked, increase a number on the screen by 1.
+
+// 2. Even or Odd Checker
+// Ask the user for a number. If it's even, display "Even". If it's odd, display "Odd".
+
+// 3. Background Color Changer
+// Make a button that changes the background color of a box each time it's clicked.
+
+// 4. Counting to 5
+// When a button is clicked, use a loop to display numbers 1 to 5 on the screen.
+
+// 5. Which Number is Bigger?
+// Ask the user for two numbers. Show the bigger number on the screen.
+
+// 6. Add Numbers Together
+// Use a loop to add numbers from 1 to 5 and show the total on the screen.
+
+// 7. Change Text Color
+// Create a button that changes the color of a sentence when clicked.
+
+// 8. Show or Hide a Message
+// Make a button that hides or shows a message when clicked.
+
+// 9. Create a Simple List
+// Ask the user for a number. Use a loop to add that many items to a list.
+
+// 10. Basic Password Check
+// Ask the user for a password. If they enter "code123", show "Access Granted". Otherwise, show "Wrong Password".
